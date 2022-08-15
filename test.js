1e203d3cf0b321226f53b5ef742ee95296b67b58
Anna Petelin <andorjamb@protonmail.com>
2022-08-15 14:37:04 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,41 @@
+
+/* function sum(x,y){
+    console.log("testing");
+    return x+y;}
+
+    console.log(sum(3,4));
+ */
+//     console.log("blue".toUpperCase());
+//     let j = "javascript";
+//     console.log(j[1]);
+
+//     let lang = "javascript";
+//     console.log(lang[lang.length-7]);
+
+//   let newLang = lang.substring(2,4);
+//   console.log(newLang);
+//   console.log(lang.substring(4));
+//   console.log(lang.substring(1));
+
+// let firstName = "Anna";
+// let lastName = "Petelin";
+// let fullName = firstName+" " + lastName;
+// console.log(fullName);
+// firstName += " Petelin";
+// console.log(firstName);
+
+// let someVariable = "inserted text";
+// let text = `an example of a template string.
+// bla bla bla, ${someVariable}`;
+// console.log(text);
+
+
+/* const newVar= "C++";
+newVar = "Javascript";
+console.log(newVar); */
+// the value of a const cannot be redefined
+
+
+
+
+
