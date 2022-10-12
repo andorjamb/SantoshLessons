98c9e66e3ed29664941c458215e719109f7caea7
Anna Petelin <adpetelin@gmail.com>
2022-10-12 21:00:49 +0300

    add checkbox to DOM now working

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -3,7 +3,7 @@ const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 let itemNumber = 0;
 const checkboxNodeList = [];
-/* const removeItem = document.querySelectorAll(); */
+
 
 
 /* ////   FUNCTIONS  ///////   */
@@ -11,25 +11,29 @@ const checkboxNodeList = [];
 
 function getAll() { /* Retreives stored list  */
     if (localStorage.length > 0) {
-        for (let i=0; i<localStorage.length;i++) {
-            localStorage.getItem(i);
+        for (let i = 0; i < localStorage.length; i++) {
+            let fetched = localStorage.getItem(i);
+            console.log(fetched);
         }
     }
 }
 
 function addItem(ob) { /* Adds new item to DOM and local storage */
     if (ob.value) {
+        /*  create checkbox */
         let checkbox = document.createElement("input");
         checkbox.setAttribute("type", "checkbox");
         checkbox.setAttribute("class", "checkbox");
-        checkbox.setAttribute("id", ob.value);
+        checkbox.setAttribute("id", ob.value.replaceAll(' ', '_'));
+        /* create new list item  */
         let newLi = document.createElement("li");
-        let newLabel = document.createElement("label");
-        newLabel.setAttribute("for", ob.value);
-        ul.appendChild(newLi);
-        newLi.appendChild(newLabel);
         newLi.textContent = ob.value;
-        newLabel.insertAdjacentElement('beforebegin', checkbox);
+        ul.appendChild(newLi);
+
+        let newLabel = document.createElement("label");
+        newLabel.setAttribute("for", ob.value.replaceAll(' ', '_'));
+        newLi.appendChild(newLabel);
+        newLabel.insertAdjacentElement('beforeend', checkbox);
         checkboxNodeList.push(checkbox);
         console.log('print checkbox list:', checkboxNodeList);
         storeItem(ob);
@@ -54,12 +58,19 @@ function resetList() { }
 /*   ////// RUN PROGRAM  //////////////  */
 
 window.onload = function () { getAll() };
+
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('submitting');
     addItem(item);
     item.value = "";
 });
+
+checkboxNodeList.addEventListener('change', function () {
+
+})
+
 
 
 
