21a3e53c986ded26ccd6d673eab9edd044f19dca
andor <andorjamb@protonmail.com>
2022-10-13 21:43:06 +0300

    input class toggles working. Need to improve storage key and removeItem functions

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -2,30 +2,21 @@ let item = document.querySelector("#item");
 const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 const reset = document.querySelector('#reset');
-const testArray = [];
 let checkboxes;
-
-
-
-
 
 /* ////   FUNCTIONS  ///////   */
 
-
 function getAll() { /* Retrieves stored list  */
     if (localStorage.length > 0) {
-        console.log(localStorage);
         for (let i = 1; i <= localStorage.length; i++) {
             let fetched = localStorage.getItem(i);
-           addItem(fetched);
+           addItem(fetched);   
         }
     }
 }
 
 function addItem(input) { /* Adds new item and checkbox to DOM */
- /* create new list item, place holder for label and checkbox  */
         let newLi = document.createElement("li");
-        //newLi.textContent = input;
         ul.appendChild(newLi);
 
         /* create label for checkbox */
@@ -40,56 +31,72 @@ function addItem(input) { /* Adds new item and checkbox to DOM */
         checkbox.setAttribute("class", "checkbox");
         checkbox.setAttribute("name", "checklist");
         checkbox.setAttribute("id", input.replaceAll(' ', '_'));
-   
-        testArray.push(checkbox);
+        checkbox.addEventListener('change', function(){
+        changeClass(checkbox.parentNode);
+        storageStatus(checkbox.checked);//if true or false
+})
+
         newLabel.insertAdjacentElement('beforeend', checkbox);  
-          
-       //checkboxes.addEventListener('change', checkFunction());
- return checkboxes = document.querySelectorAll('.checkbox');
-    
+ return checkboxes = document.querySelectorAll('.checkbox');   
 }
 
 function storeItem(input) {  /* adds item to localStorage, updates list length */
+/**change this! make the key the input id. then the value can be true or false! */
     localStorage.setItem(((localStorage.length)+1), input);
-   
 }
 
-function removeItem(ob) {
-    testArray.pop(ob);
-    localStorage.remove(ob);
+/**There is a problem here! You need to fix the parameter */
+function removeItem(ob.value()) {
+    localStorage.removeItem(ob);
 }
-
 
 function resetList() {
     localStorage.clear();
+    ul.innerHTML='';
 }
 
-function checkFunction(){
-let helpMe = document.querySelectorAll('.checkbox');     
-console.log("helpMe");
+function changeClass(e){
+    e.parentNode.classList.toggle('strike-through')
 }
 
+function storageStatus(bool){
+    console.log('store status output:', bool);
+    if(bool){
+        removeItem();
+    }
+    else{storeItem()}
+
+}
 
 /*   ////// RUN PROGRAM  //////////////  */
 
-window.onload = () => getAll();
+window.onload = () => {
+    getAll();
+    if (checkboxes){
+    checkboxes.forEach((node)=>{
+        node.addEventListener('change', function(){        
+        console.log('event listener working!');
+        changeClass(node);
+        storageStatus(node.checked);//if true or false
+    })
+    });
+    }
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();  
-if (item.value){ addItem(item.value);
-    /* let checkboxes = document.querySelectorAll('.checkbox');
-    console.log('testArray: ', testArray)
-    console.log(checkboxes); */
+if (item.value){ 
+    addItem(item.value);
     storeItem(item.value);
     item.value = "";
-    console.log('checkboxes: ', checkboxes);
-    checkboxes.forEach((node)=>{
-    node.addEventListener('change', function(){console.log(node)})});
 }
-});
+    }
+    ); 
 
-
-reset.addEventListener('click', ()=>{resetList();
-    return (console.log('local storage cleared.'))});
+reset.addEventListener('click', ()=>{
+    resetList();
+    return (console.log('local storage cleared.'))
+}
+);
 
 
