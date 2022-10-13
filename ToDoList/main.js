86bbe874095b4c8f4babcf3f86232aa6e7586ba5
andor <andorjamb@protonmail.com>
2022-10-13 13:45:41 +0300

    list repopulation working

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -2,47 +2,49 @@ let item = document.querySelector("#item");
 const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 let itemNumber = 0;
-const checkboxNodeList = [];
+const checkedNodes = [];
+const checkboxNodeList = document.querySelector('.checkbox');
+const reset = document.querySelector('#reset');
 
 
 
 /* ////   FUNCTIONS  ///////   */
 
 
-function getAll() { /* Retreives stored list  */
+function getAll() { /* Retrieves stored list  */
     if (localStorage.length > 0) {
-        for (let i = 0; i < localStorage.length; i++) {
+        console.log(localStorage);
+        for (let i = 1; i <= localStorage.length; i++) {
             let fetched = localStorage.getItem(i);
             console.log(fetched);
+           addItem(fetched);
         }
     }
 }
 
-function addItem(ob) { /* Adds new item to DOM and local storage */
-    if (ob.value) {
+
+function addItem(input) { /* Adds new item and checkbox to DOM */
         /*  create checkbox */
         let checkbox = document.createElement("input");
         checkbox.setAttribute("type", "checkbox");
         checkbox.setAttribute("class", "checkbox");
-        checkbox.setAttribute("id", ob.value.replaceAll(' ', '_'));
+        checkbox.setAttribute("id", input.replaceAll(' ', '_'));
         /* create new list item  */
         let newLi = document.createElement("li");
-        newLi.textContent = ob.value;
+        newLi.textContent = input;
         ul.appendChild(newLi);
 
+        /* create empty label for checkbox */
         let newLabel = document.createElement("label");
-        newLabel.setAttribute("for", ob.value.replaceAll(' ', '_'));
+        newLabel.setAttribute("for", input.replaceAll(' ', '_'));
         newLi.appendChild(newLabel);
         newLabel.insertAdjacentElement('beforeend', checkbox);
-        checkboxNodeList.push(checkbox);
-        console.log('print checkbox list:', checkboxNodeList);
-        storeItem(ob);
-
-    }
+        console.log('print checkboxnode list:', checkboxNodeList);
+    
 }
 
-function storeItem(ob) {
-    localStorage.setItem(++itemNumber, ob.value);
+function storeItem(input) {  /* adds item to localStorage, updates list length */
+    localStorage.setItem(++itemNumber, input);
     return itemNumber;
 }
 
@@ -51,29 +53,37 @@ function removeItem(ob) {
 }
 
 
-
-function resetList() { }
+function resetList() { 
+    localStorage.clear();
+}
 
 
 /*   ////// RUN PROGRAM  //////////////  */
 
-window.onload = function () { getAll() };
-
+window.onload = () => getAll();
+console.log(localStorage.getItem("0"));
+console.log(checkboxNodeList);
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('submitting');
-    addItem(item);
-    item.value = "";
+if (item.value){ addItem(item.value);
+    storeItem(item.value);
+    item.value = "";}
 });
 
-checkboxNodeList.addEventListener('change', function () {
+reset.addEventListener('click', ()=>resetList());
 
-})
+for (let node in checkboxNodeList){
+    console.log(node);
+    console.log('class list', checkbox.classList);
+    node.addEventListener('change', function () {
+        if (node.checked) {node.classList.add("strike-through");}
+        else {
+            node.classList.remove("strike-through");}
+ 
 
-
-
-
+})}
 
 
 
